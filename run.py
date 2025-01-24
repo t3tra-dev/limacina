e6390cf30e114d757dda2fe8059b8c5a319a497e
t3tra-dev <admin@t3tra.net>
2025-01-24 17:58:29 +0900

    prod

--- run.py
+++ run.py
@@ -50,6 +50,6 @@ def create_app():
 
 
 if __name__ == "__main__":
-    load_dotenv(".env")
+    load_dotenv()
     app = create_app()
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=80)
